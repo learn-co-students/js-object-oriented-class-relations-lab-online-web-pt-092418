4d8267b5015cdaaaf60eb904393d19b259513565
Virginia Dooley <vcdooley@gmail.com>
2019-04-28 22:03:26 +0100

    Done.

---
+++ index.js
@@ -0,0 +1,48 @@
+let store = { drivers: [], passengers: [], trips: []};
+
+let driverId = 0;
+let passengerId = 0;
+let tripId = 0;
+
+class Driver {
+  constructor(name) {
+    this.id = ++driverId;
+    this.name = name;
+
+    store.drivers.push(this);
+  };
+}
+
+class Passenger {
+  constructor(name) {
+    this.id = ++driverId;
+    this.name = name;
+
+    store.passengers.push(this);
+  };
+}
+
+class Trip {
+  constructor(driver, passenger) {
+    this.id = ++tripId;
+    this.driverId = driver.id;
+    this.passengerId = passenger.id;
+
+    store.trips.push(this);
+  }
+
+  passenger() {
+    return store.passengers.find(
+      function(passenger) {
+        return passenger.id === this.passengerId;
+      }.bind(this));
+   }
+
+   driver() {
+     return store.drivers.find(
+       function(driver) {
+         return driver.id === this.driverId;
+       }.bind(this));
+    }
+
+ }
